09309bcc266fe2112d627539f6c730938d095e08
Khoi <8buianhkhoi@gmail.com>
2022-12-13 09:14:31 +0700

    update index page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Routes, Route } from 'react-router-dom'
-import { useTranslation } from 'react-i18next'
+import { Routes, Route } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
-import './App.css';
-import Header from './Components/Headers';
-import Footer from './Components/Footer';
-import Contact from './Components/Contact';
-import AllProducts from './Components/Products/AllProducts/AllProducts';
-import HomePage from './Components/Homepage';
+import "./App.css";
+import Header from "./Components/Headers";
+import Footer from "./Components/Footer";
+import Contact from "./Components/Contact";
+import AllProducts from "./Components/Products/AllProducts/AllProducts";
+import HomePage from "./Components/Homepage";
 
 function App() {
   const { t } = useTranslation();
@@ -15,9 +15,13 @@ function App() {
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path="/" element={<HomePage title={t('homepageHeaderMenu')} />}></Route>
-        <Route path="/contact" element={<Contact/>}></Route>
-        <Route path="/allproducts" element={<AllProducts/>}></Route>
+        <Route
+          index
+          path="/"
+          element={<HomePage title={t("homepageHeaderMenu")} />}
+        ></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/allproducts" element={<AllProducts />}></Route>
       </Routes>
       <Footer></Footer>
     </div>
